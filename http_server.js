74bbffcc223f43550c1fd8282bfa24d696feb8c1
Zainab <zainabintech@gmail.com>
2021-12-25 23:56:21 -0500

    added http file

---
+++ http_server.js
@@ -0,0 +1,11 @@
+// Include http module.
+var http = require('http');
+// Create http server.
+var httpServer = http.createServer(function (req, resp) {
+    resp.writeHead(200, {'Access-Control-Allow-Origin':'*','Content-Type': 'text/plain'});
+    resp.write("Welcome to dev2qa.com.");
+    resp.end();
+});
+// Start http server listen on port 8888.
+httpServer.listen(8888);
+console.log("Use browser to get url 'http://localhost:8888/http_server.js'");
